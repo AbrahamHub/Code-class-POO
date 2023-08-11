54851f302c0c39d234b6ba81b98860e01e52a33b
AbrahamHub <Apple_Abraham@hotmail.com>
2023-08-10 23:45:42 -0600

    #5

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,5 +1,5 @@
 public class Doctor {
-    int id;
+    static int id=0; // Autoincrement
     String name;
     String speciality;
 
@@ -8,8 +8,13 @@ public class Doctor {
     }
     Doctor() {
         System.out.println("Construyendo el objeto Doctor");
+        id++;
     }
     Doctor (String name) {
         System.out.println("El nombre del doctor asignado es: " + name);
+        id++;
+    }
+    public void showId() {
+        System.out.println("ID Doctor: " + id);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,18 @@ public class Main {
         Doctor myDoctor1 = new Doctor();
         myDoctor1.name = "Abraham";
         myDoctor1.showName();
+        myDoctor1.showId();
 
         Doctor myDoctor2 = new Doctor();
         myDoctor2.name = "Oscar";
         myDoctor2.showName();
+        myDoctor2.showId();
+
+        //Doctor.id++;
 
         Doctor myDoctor3 = new Doctor();
         myDoctor3.name = "Ricardo";
         myDoctor3.showName();
+        myDoctor3.showId();
     }
 }
