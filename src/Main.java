966743622929c2e32b44fe035a48267fd94b287b
AbrahamHub <Apple_Abraham@hotmail.com>
2023-08-10 23:59:45 -0600

    #7

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,6 @@ import static ui.UIMenu.showMenu;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-        Doctor myDoctor1 = new Doctor();
-        myDoctor1.name = "Abraham";
-        myDoctor1.showName();
-        myDoctor1.showId();
-
-        Doctor myDoctor2 = new Doctor();
-        myDoctor2.name = "Oscar";
-        myDoctor2.showName();
-        myDoctor2.showId();
-
-        //Doctor.id++;
-
-        Doctor myDoctor3 = new Doctor();
-        myDoctor3.name = "Ricardo";
-        myDoctor3.showName();
-        myDoctor3.showId();
-
         showMenu();
     }
 }
