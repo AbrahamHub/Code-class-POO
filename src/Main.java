0531ce60e385855e33b906cf32faade71f37f77f
AbrahamHub <apple_abraham@hotmail.com>
2023-08-29 18:48:39 -0600

    #9

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import static ui.UIMenu.showMenu;
-
 public class Main {
     public static void main(String[] args) {
         //showMenu();
---
+++ src/Patient.java
@@ -0,0 +1,15 @@
+public class Patient {
+    public String name;
+    public String email;
+    public String address;
+    public String phoneNumber;
+    public String birthday;
+    public double weight;
+    public double height;
+    public String blood;
+
+    Patient(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
