748d8a301a7dc7d5b1ab487aeacde2d711dab5fd
AbrahamHub <apple_abraham@hotmail.com>
2023-08-29 20:55:55 -0600

    #12

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Doctor {
     static int id=0; // Autoincrement
-    String name;
-    String email;
-    String speciality;
+    private String name;
+    private String email;
+    private String speciality;
 
     public void showName() {
         System.out.println(name);
@@ -20,4 +23,48 @@ public class Doctor {
     public void showId() {
         System.out.println("ID Doctor: " + id);
     }
-}
+
+
+    ArrayList<AviableAppointment> availableAppointments = new ArrayList<>();
+    public void addAvailableAppointment(Date date, String time) {
+        availableAppointments.add(new Doctor.AviableAppointment(date, time));
+    }
+    public ArrayList<AviableAppointment> getAvailableAppointments() {
+        return availableAppointments;
+    }
+
+    public static class AviableAppointment {
+        private int id;
+        private Date date;
+        private String time;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
+
+        public AviableAppointment(Date date, String time) {
+            this.date = date;
+            this.time = time;
+        }
+
+    }}
